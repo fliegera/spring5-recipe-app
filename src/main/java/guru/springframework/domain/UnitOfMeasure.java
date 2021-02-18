2bfb04e1e678ddd30c87788aed6ee0bb90a82614
Andrej Flieger <AFG@host.docker.internal>
2021-02-18 10:39:53 +0100

    Adding OneToOne

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,54 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String uom;
+
+	public UnitOfMeasure() {
+		super();
+	}
+
+	public UnitOfMeasure(Long id, String uom) {
+		super();
+		this.id = id;
+		this.uom = uom;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the uom
+	 */
+	public String getUom() {
+		return uom;
+	}
+
+	/**
+	 * @param uom the uom to set
+	 */
+	public void setUom(String uom) {
+		this.uom = uom;
+	}
+
+}
