db15fb8e35709863555a45e4244dcbef166e331c
Andrej Flieger <AFG@host.docker.internal>
2021-02-18 11:48:00 +0100

    Adding findByDescription mthods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -15,8 +16,24 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        return "index";
-    }
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		Optional<Category> category = categoryRepository.findByDescription("American");
+
+		Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Cat ID is " + category.get().getId());
+		System.out.println("Uom ID is " + uom.get().getId());
+
+		return "index";
+	}
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
 }
